b9d51f688f1a10311718c346156b571d3c0f531a
habibiefattahillah <habibiefattahillah78@gmail.com>
2025-06-09 01:59:48 +0700

    okay?

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import DataInput from "./(input)/AppInput";
-import ResultTable from "./(result-table)/ResultTable";
 
 export default async function App() {
     return (
         <div className="grid gap-y-3">
             <DataInput />
-            <ResultTable />
         </div>
     );
 }
